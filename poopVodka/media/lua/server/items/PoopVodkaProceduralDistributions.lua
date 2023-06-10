ded0ac70539dae857f9dc5d41bd72de63e3ede0a
Kacper Czernik <Kacper-czernik@wp.pl>
2023-06-10 19:11:34 +0200

    PoopVodka | found out distributions =/= proceduralDistribution, changed proceduralDistribution so poopVodka will spanw in game

---
+++ poopVodka/media/lua/server/items/PoopVodkaProceduralDistributions.lua
@@ -0,0 +1,327 @@
+ProceduralDistributions = {};
+
+ProceduralDistributions.list = {
+
+    BandPracticeFridge = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 8,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    BarCounterLiquor = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 8,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+            "poopVodka.poopVodka", 8,
+            }
+        }
+    },
+
+    BarShelfLiquor = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 8,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    CabinetFactoryTools = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 4,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    CarSupplyTools = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 4,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    ChangeroomCounters = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 1,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    ClassroomDesk = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 4,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    CrateCamping = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 1,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    CrateFarming = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 2,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    CrateFishing = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 3,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    CrateMechanics = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 1,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    CrateRandomJunk = {
+        rolls = 1,
+        items = {
+            "poopVodka.poopVodka", 0.01,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    DrugShackMisc = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 1,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    FactoryLockers = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 0.5,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    FireDeptLockers = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 0.01,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    FridgeGeneric = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 0.8,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    FridgeTrailerPark = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 0.8,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    GarageMechanics = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 0.3,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    GasStorageMechanics = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 0.2,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    GymLockers = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 0.1,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    JanitorMisc = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 8,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    MechanicShelfMisc = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 0.1,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    SchoolLockers = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 0.4,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+    StoreShelfCombo = {
+        rolls = 4,
+        items = {
+            "poopVodka.poopVodka", 1,
+        },
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        }
+    },
+
+}
+--
+--    all = clothingStores;
+--
+--    clothesstore = clothingStores;
+
+
+--table.insert(ProceduralDistributions.list, distributionTable);
+
+--for mod compat:
+--ProceduralDistributions = distributionTable;
