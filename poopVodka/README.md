3212f1e35af854796f151d40c3efa5999033e01a
Kacper Czernik <Kacper-czernik@wp.pl>
2023-06-12 20:20:13 +0200

    poopVodka | now spawns in world (empty bottle in trash), bottle is now reusable | repository | fixed typos in README

--- poopVodka/README.md
+++ poopVodka/README.md
@@ -1,13 +1,18 @@
-goal: 
-	create a item based on a fat lord lore called "skórwolańska wódka z gówna"
+goal:   
+	create a item based on a fat lord lore called "skórwolańska wódka z gówna"  
+  
+to do:  
+	#1 learn how to mod  
+		#1.1 item exist ✓  
+		#1.2 distribution ✓  
+		#1.3 bottle can be empty and filled with water ✓  
+		#1.4 bottle can be weponized  
+		#1.5 optimalizaction and balancing  
+	#2 succes  
+  
+description:  
+	Item based on a fat lord lore called "skórwolańska wódka z gówna", the strongest alien alcohol (7%).  
+	The legends said that after drinking you can feel like on a heavenly Skurwoala.  
+  
+notes:  
 
-to do:
-	#1 learn how to mod
-	#2 succes
-
-description:
-	Item based on a fat lord lore called "skórwolańska wódka z gówna", the strongest alien alcohol (7%).
-	The legends said that after drinking you can feel like on a heavenly Skurwoala.
-
-notes:
-in mod.info there is a tmp.png, need to change it's name
---
+++ poopVodka/media/lua/server/items/poopVodkaDistribution.lua
@@ -0,0 +1,61 @@
+require "Items/Distributions"
+require "Items/ItemPicker"
+require"Items/ProceduralDistribution"
+
+--vodka botle
+
+table.insert(ProceduralDistributions["list"]["BarShelfLiquor"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["BarShelfLiquor"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["CrateMechanics"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["CrateMechanics"].items, 0.3)
+
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["BarCounterLiquor"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["BarCounterLiquor"].items, 0.5)
+
+table.insert(ProceduralDistributions["list"]["BarCratePool"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["BarCratePool"].items, 0.5)
+
+table.insert(ProceduralDistributions["list"]["FishingStoreGear"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["FishingStoreGear"].items, 0.5)
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["CarSupplyTools"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["CarSupplyTools"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, 0.8)
+
+table.insert(ProceduralDistributions["list"]["FactoryLockers"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["FactoryLockers"].items, 2)
+
+table.insert(ProceduralDistributions["list"]["FishingStoreBait"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["FishingStoreBait"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["JanitorTools"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["JanitorTools"].items, 2.1)
+
+table.insert(ProceduralDistributions["list"]["JanitorChemicals"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["JanitorChemicals"].items, 0.3)
+
+table.insert(ProceduralDistributions["list"]["MechanicSpecial"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["MechanicSpecial"].items, 0.5)
+
+table.insert(ProceduralDistributions["list"]["SecurityLockers"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["SecurityLockers"].items, 1.8)
+
+table.insert(ProceduralDistributions["list"]["GarageTools"].items, "poopVodka.PoopVodka")
+table.insert(ProceduralDistributions["list"]["GarageTools"].items, 2.4)
+
+--empty botle
+
+table.insert(ProceduralDistributions["list"]["JunkBin"].items, "poopVodka.PoopVodkaEmpty")
+table.insert(ProceduralDistributions["list"]["JunkBin"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["JunkHoard"].items, "poopVodka.PoopVodkaEmpty")
+table.insert(ProceduralDistributions["list"]["JunkHoard"].items, 0.8)
