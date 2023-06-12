a0c2c6fe051c80c9f7e7e4096b277eb09fb59d83
Kacper Czernik <57114894+KapravveOczko@users.noreply.github.com>
2023-06-12 18:42:40 +0200

    poopVodka | distribution fixed, test fase 2 starts

--- poopVodka/media/lua/server/items/testDistribution.lua
+++ poopVodka/media/lua/server/items/testDistribution.lua
@@ -4,3 +4,48 @@ require"Items/ProceduralDistribution"
 
 table.insert(ProceduralDistributions["list"]["BarShelfLiquor"].items, "poopVodka.poopVodka")
 table.insert(ProceduralDistributions["list"]["BarShelfLiquor"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["CrateMechanics"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["CrateMechanics"].items, 0.3)
+
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["BarCounterLiquor"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["BarCounterLiquor"].items, 0.5)
+
+table.insert(ProceduralDistributions["list"]["BarCratePool"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["BarCratePool"].items, 0.5)
+
+table.insert(ProceduralDistributions["list"]["FishingStoreGear"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["FishingStoreGear"].items, 0.5)
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["CarSupplyTools"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["CarSupplyTools"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, 0.8)
+
+table.insert(ProceduralDistributions["list"]["FactoryLockers"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["FactoryLockers"].items, 2)
+
+table.insert(ProceduralDistributions["list"]["FishingStoreBait"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["FishingStoreBait"].items, 1)
+
+table.insert(ProceduralDistributions["list"]["JanitorTools"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["JanitorTools"].items, 2.1)
+
+table.insert(ProceduralDistributions["list"]["JanitorChemicals"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["JanitorChemicals"].items, 0.3)
+
+table.insert(ProceduralDistributions["list"]["MechanicSpecial"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["MechanicSpecial"].items, 0.5)
+
+table.insert(ProceduralDistributions["list"]["SecurityLockers"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["SecurityLockers"].items, 1.8)
+
+table.insert(ProceduralDistributions["list"]["GarageTools"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["GarageTools"].items, 2.4)
