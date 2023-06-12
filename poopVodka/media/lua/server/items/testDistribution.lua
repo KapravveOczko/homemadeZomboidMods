167f09754461a05b1299ebe397ceb4fa1b990f76
Kacper Czernik <57114894+KapravveOczko@users.noreply.github.com>
2023-06-12 11:23:55 +0200

    poopVodka | old distribution ended in tmp directory | new oversimplevied spawning added, with works, at least does not destroy item spawning

---
+++ poopVodka/media/lua/server/items/testDistribution.lua
@@ -0,0 +1,6 @@
+require "Items/Distributions"
+require "Items/ItemPicker"
+require"Items/ProceduralDistribution"
+
+table.insert(ProceduralDistributions["list"]["BarShelfLiquor"].items, "poopVodka.poopVodka")
+table.insert(ProceduralDistributions["list"]["BarShelfLiquor"].items, 1)
